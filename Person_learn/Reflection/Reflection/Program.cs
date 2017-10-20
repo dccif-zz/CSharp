f6469c6f3ae61f82c063f8aa0ba7911d01db6bd6
dccif <heyujieboy@163.com>
2017-10-20 23:53:50 +0800

    Personal CSharp learn

---
+++ Person_learn/Reflection/Reflection/Program.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Reflection;
+
+namespace BugFixApplication
+{
+    [AttributeUsage(AttributeTargets.Class |
+                    AttributeTargets.Constructor |
+                    AttributeTargets.Field |
+                    AttributeTargets.Method |
+                    AttributeTargets.Property, AllowMultiple = true)]
+    public class DebugInfo : System.Attribute
+    {
+        private int bugNo;
+        private string developer;
+        private string lastReview;
+        public string message;
+
+        public DebugInfo(int bg, string dev, string d)
+        {
+            this.bugNo = bg;
+            this.developer = dev;
+            this.lastReview = d;
+        }
+
+        public int BugNo { get { return bugNo; } }
+        public string Developer { get { return developer; } }
+        public string LastReview { get { return lastReview; } }
+        public string Message { get { return message; } set { message = value; } }
+    }
+
+    [DebugInfo(45, "Zara Ali", "12/8/2012",
+        Message = "Return type mismatch")]
+    [DebugInfo(49, "Nuha Ali", "10/10/2012",
+        Message = "Unused variable")]
+    class Rectangle
+    {
+        protected double length;
+        protected double width;
+
+        public Rectangle(double l, double w)
+        {
+            length = l;
+            width = w;
+        }
+
+        [DebugInfo(55, "Zara Ali", "19/10/2012",
+            Message = "Return type mismatch")]
+        public double GetArea()
+        {
+            return length * width;
+        }
+
+        [DebugInfo(56, "Zara Ali", "19/10/2012")]
+        public void Display()
+        {
+            Console.WriteLine("Length: {0}", length);
+            Console.WriteLine("Width: {0}", width);
+            Console.WriteLine("Area: {0}", GetArea());
+        }
+    }
+
+    class Exxecute
+    {
+        static void Main(string[] args)
+        {
+            Rectangle r = new Rectangle(4.5,7.5);
+            r.Display();
+            Type type = typeof(Rectangle);
+            foreach (object attributes in type.GetCustomAttributes(false))
+            {
+                DebugInfo dbi = (DebugInfo) attributes;
+                if (null != dbi)
+                {
+                    Console.WriteLine("Bug no: {0}",dbi.BugNo);
+                    Console.WriteLine("Developer: {0}",dbi.Developer);
+                    Console.WriteLine("Last Reviewed: {0}",dbi.LastReview);
+                    Console.WriteLine("Remarks: {0}",dbi.Message);
+                }
+            }
+
+            foreach (MethodInfo m in type.GetMethods())
+            {
+                foreach (Attribute a in m.GetCustomAttributes(true))
+                {
+                    DebugInfo dbi = (DebugInfo) a;
+                    if (null != dbi)
+                    {
+                        Console.WriteLine("Bug no : {0},for Method: {1}",dbi.BugNo,m.Name);
+                        Console.WriteLine("Developer:{0}",dbi.Developer);
+                        Console.WriteLine("Last Reviewed:{0}",dbi.LastReview);
+                        Console.WriteLine("Remarks:{0}",dbi.Message);
+                    }
+                }
+            }
+            Console.ReadLine();
+        }
+    }
+
+}
---
+++ Person_learn/Thread/Thread/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+namespace ThreadApplication
+{
+    class ThreadCreation
+    {
+        public static void CallToChildThread()
+        {
+            Console.WriteLine("Child thread starts");
+        }
+
+        static void Main(string[] args)
+        {
+            ThreadStart childref = new ThreadStart(CallToChildThread);
+            Console.WriteLine("In Main: Creating the Child thread");
+            Thread chilThread = new Thread(childref);
+            chilThread.Start();
+            Console.ReadKey();
+        }
+    }
+}
